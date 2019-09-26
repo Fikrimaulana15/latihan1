fe801f40e72f0541ebd5ed16988e38ee13d7ae70
ASUS <ASUS@192.168.1.10>
2019-09-26 14:58:55 +0700

    latihan1

---
+++ src/pbo2/pkg10118074/latihan1/biodata/PBO210118074Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | 
+Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM : 10118074
+ */
+public class PBO210118074Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"----------------------------------");
+        System.out.println("NAMA\t\t: Fikri Maulana Amir");
+        System.out.println("ALAMAT\t\t: ja;an babakan cianjur");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 15 Januaari 2000");
+        System.out.println("------------------------------------");
+        System.out.println("HOBI\t\t:futsal & touring ");
+        System.out.println("MAKES\t\t: nasi padang ");
+        System.out.println("MIKES\t\t: jus");
+    }
+    
+    
+    
+}
